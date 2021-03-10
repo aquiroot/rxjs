54a28601f56db187b261224521b667d4f082cf18
Damian Colaneri <dcolaneri@gmail.com>
2021-03-10 17:24:15 -0300

    [ADD] mergeMap

---
+++ src/operadores-transformacion/01-mergeAll.ts
@@ -0,0 +1,46 @@
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { debounceTime, map, mergeAll, pluck } from 'rxjs/operators';
+import { GithubUser } from './interfaces/githubUsers.interfeaces';
+// Referencias
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+body.append(textInput, orderList);
+
+// Helpers
+const mostrarUsuarios = (usuarios: GithubUser[]) => {
+	orderList.innerHTML = '';
+
+	for (const usuario of usuarios) {
+		const li = document.createElement('li');
+		const img = document.createElement('img');
+		img.src = usuario.avatar_url;
+
+		const anchor = document.createElement('a');
+		anchor.href = usuario.html_url;
+		anchor.text = 'ver pagina';
+		anchor.target = '_blank';
+
+		li.append(img);
+		li.append(usuario.login + ' ');
+		li.append(anchor);
+
+		orderList.append(li);
+	}
+};
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
+
+input$
+	.pipe(
+		debounceTime(1000),
+		pluck('target', 'value'),
+		map((texto) =>
+			ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)
+		),
+		mergeAll(),
+		pluck<any, GithubUser[]>('items')
+	)
+	.subscribe(mostrarUsuarios);
