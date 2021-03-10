11ccfa01895be0b7255153722baa5f8bc98de55e
Damian Colaneri <dcolaneri@gmail.com>
2021-03-09 22:38:03 -0300

    inicio de operadores de transformacion

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { debounceTime, map, mergeAll, pluck } from 'rxjs/operators';
+// Referencias
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+body.append(textInput, orderList);
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
+
+input$
+	.pipe(
+		debounceTime(1000),
+		pluck('target', 'value'),
+		map((texto) =>
+			ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)
+		),
+		mergeAll(),
+		pluck('items')
+	)
+	.subscribe((resp) => console.log(resp));
