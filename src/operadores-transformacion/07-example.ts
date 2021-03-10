8b734e6835c990dd3845a843f447bbedd9f414fe
Damian Colaneri <dcolaneri@gmail.com>
2021-03-10 18:57:05 -0300

    [ADD] example

---
+++ src/operadores-transformacion/07-example.ts
@@ -0,0 +1,53 @@
+import { fromEvent, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import {
+	tap,
+	map,
+	mergeMap,
+	switchMap,
+	exhaustMap,
+	pluck,
+	catchError,
+} from 'rxjs/operators';
+
+// Items para crear formulario
+const form = document.createElement('form');
+const inputEmail = document.createElement('input');
+const inputPass = document.createElement('input');
+const submitBtn = document.createElement('button');
+
+// Configuraciones
+inputEmail.type = 'email';
+inputEmail.placeholder = 'Email';
+inputEmail.value = 'eve.holt@reqres.in';
+
+inputPass.type = 'password';
+inputPass.placeholder = 'Password';
+inputPass.value = 'cityslicka';
+
+submitBtn.innerHTML = 'Ingresar';
+
+// insertar al body
+form.append(inputEmail, inputPass, submitBtn);
+document.querySelector('body').append(form);
+
+// Helpers
+const peticionHttpLogin = (userPass) =>
+	ajax.post('https://reqres.in/api/login?delay=1', userPass).pipe(
+		pluck('response', 'token'),
+		catchError((err) => of('err:', err))
+	);
+
+// Streams
+const submitForm$ = fromEvent(form, 'submit').pipe(
+	tap((ev) => ev.preventDefault()),
+	map((ev) => ({
+		email: ev.target[0].value,
+		password: ev.target[1].value,
+	})),
+	exhaustMap(peticionHttpLogin)
+);
+
+submitForm$.subscribe((token) => {
+	console.log(token);
+});
