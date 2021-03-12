1dfd84b913a1ef7862660350200cb6554a2c6e37
Damian Colaneri <dcolaneri@gmail.com>
2021-03-11 23:34:10 -0300

    [ADD] solucion de practica 01

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,22 @@
-/**
- * Ejercicio:
- * El objetivo de es realizar la misma impresión, pero usando observables
- * Nota: NO hay que usar el ciclo "FOR OF", usar un observable y llamar la función capitalizar
- */
+import { from } from 'rxjs';
 
 /**
- * Salida esperada:
- * Batman
- * Joker
- * Doble Cara
- * Pingüino
- * Hiedra Venenosa
+ * Ejercicio:
+ * Sume todos los números del arreglo usando un reduce.
+ * Debe de filtrar para que sólo números sean procesados
+ * La salida debe de ser 32
+ *
+ * Tip:
+ * isNan() es una función de JavaScript para determinar si es número
+ * Usar filter<any>(...) para no tener problemas de tipado.
  */
 
 (() => {
-	const nombres = [
-		'batman',
-		'joker',
-		'doble cara',
-		'pingüino',
-		'hiedra venenosa',
-	];
+	const datos = [1, 2, 'foo', 3, 5, 6, 'bar', 7, 8];
 
-	const capitalizar = (nombre: string) =>
-		nombre.replace(
-			/\w\S*/g,
-			(txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-		);
-
-	// Cambiar este FOR OF, por un observable y capitalizar las emisiones
-	for (let nombre of nombres) {
-		console.log(capitalizar(nombre));
-	}
+	from(datos)
+		.pipe
+		// Trabajar aquí
+		()
+		.subscribe(console.log); // La salida debe de ser 32
 })();
