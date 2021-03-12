c1d79c57e81cf5a88b1fb0068960fddd4c1f4473
Damian Colaneri <dcolaneri@gmail.com>
2021-03-11 23:12:25 -0300

    [ADD] forkJoin example

---
+++ src/metodos/07-example-forkJoin.ts
@@ -0,0 +1,16 @@
+import { forkJoin, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError } from 'rxjs/operators';
+
+const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_USER = 'aquiroot';
+
+forkJoin({
+	usuario: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}`),
+	repos: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/repos`),
+	gist: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/gists`),
+})
+	.pipe(catchError((err) => of(err.message)))
+	.subscribe(console.log);
+
+// hace 3 peticiones a la API y cuando obtiene todos los datos devuelve un objeto con la info
